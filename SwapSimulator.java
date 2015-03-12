2f953b0f738ddec3d463fe4107c2d8d7df411c06
mrjbar <jabari.lofton@sjsu.edu>
2015-03-12 12:13:55 -0700

    add printMemoryMap(), decrementProcess(), add()

--- SwapSimulator.java
+++ SwapSimulator.java
@@ -4,32 +4,74 @@ public class SwapSimulator {
 	
 	LinkedList<Process> memory = new LinkedList<Process>();
 	
-	public void firstFit()
+	public SwapSimulator()
 	{
-		ArrayList<Process> processQueue = new ArrayList<Process>();
+		memory.add(new Process(0, 100, 0, 0, false, true));
+	}
+	
+	public void firstFit(ArrayList<Process> processQueue)
+	{
+		printMemoryMap();
 		// Create some processes
-		for(int i=0; i<10; i++)
+		for(int i=0; i<processQueue.size(); i++)
 		{
-			processQueue.add(new Process(i, ))
+			if(add(processQueue.get(i)))
+			{
+				printMemoryMap();
+			}
+			// Add processes
+			// Decrement processes
+			// Check if processes are 0
+			// remove them from memory
 		}
-		
 	}
 	
-	public void add(Process p)
+	public boolean add(Process process)
 	{
-		memory.add(p);
+		ListIterator<Process> listIterator = memory.listIterator();
+        while (listIterator.hasNext()) {
+        	Process proc = listIterator.next();
+        	
+            if(proc.isHole() && (proc.size >= process.size))
+            {
+            	// Split the empty hole in half
+            	proc.size = proc.size - process.size;
+            	// Replace the hole with the current process
+            	listIterator.set(process);
+            	// Add the left over hole after this process
+            	listIterator.add(proc);
+            	return true;
+            }        	
+        }     
+        return false;
 	}
 	
-	public int size()
+	public void decrementProcess(Process proc)
 	{
-	        int size =  (int)(Math.random()*4);
-	        switch(size)
-	        {
-	        	case 0: return 5;
-	        	case 1: return 11;
-	        	case 2: return 17;
-	        	default: return 31;
-	        }
+		if(!proc.isHole())
+		{
+			if(proc.getDuration() != 0)
+			{
+				proc.setDuration(proc.getDuration() - 1);
+			}
+			else
+			{
+				proc.setHole(true);
+			}
+		}
 	}
+	
+	public void printMemoryMap()
+	{
+		ListIterator<Process> listIterator = memory.listIterator();
+		while (listIterator.hasNext()) {
+        	Process proc = listIterator.next();
+        	if(proc.isHole())
+        		System.out.print("[Hole => " + proc.getSize() + "MB], ");
+        	else
+        		System.out.print("[P" + proc.id + " => " + proc.getSize() + "MB], ");
 
+		}
+		System.out.println("\n");
+	}
 }
