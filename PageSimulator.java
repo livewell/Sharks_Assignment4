1bbe81460b0655b3208b6d753b3b958dfd75ef50
mrjbar <jabari.lofton@sjsu.edu>
2015-03-14 13:32:45 -0700

    Completed nextPage() which will generate the next page reference

--- PageSimulator.java
+++ PageSimulator.java
@@ -3,11 +3,14 @@ import java.util.*;
 public class PageSimulator {
 	
 	static int i = 0;
-	public int getNextPage()
+	public int nextPage()
 	{
-		int r =  (int)(Math.random()*9);
-		i = r;
+		int[] lor = {-1, 0, 1};
+		int r =  (int)(Math.random()*10);
+		if(r >= 0 && r < 7)
+			i = (i + lor[(int)(Math.random()*3)]) % 9;
+		if(r >= 7 && r<= 9)
+			i = (int)(2 + Math.random()*6);
 		return i;
 	}
-
 }
