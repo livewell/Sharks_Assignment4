68665e8097d38caaf7af718178bfe92f28f13471
mrjbar <jabari.lofton@sjsu.edu>
2015-03-14 14:09:05 -0700

    Fixed the nextPage to ensure the page references will wrap from 0 to 9.

--- PageSimulator.java
+++ PageSimulator.java
@@ -8,9 +8,19 @@ public class PageSimulator {
 		int[] lor = {-1, 0, 1};
 		int r =  (int)(Math.random()*10);
 		if(r >= 0 && r < 7)
-			i = (i + lor[(int)(Math.random()*3)]) % 9;
+		{
+			// Generate a random value r from 0 to 2
+			// Place r in the locality of reference array to get value l: -1 , 0, or 1
+			// Add l onto the last page reference i and modulo 9 the result to get a value within 0 - 9
+			i = (i + lor[(int)(Math.random()*3)]) % 10;
+			if(i<0)
+				i = 9;
+		}
 		if(r >= 7 && r<= 9)
-			i = (int)(2 + Math.random()*6);
+		{
+			//Generate values 2-8 inclusive
+			i = (int)(2 + Math.random()*7); 
+		}
 		return i;
 	}
 }
