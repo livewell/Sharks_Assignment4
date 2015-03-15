895374183395c63e46f621c28b40d814deb67c4a
mrjbar <jabari.lofton@sjsu.edu>
2015-03-15 14:06:39 -0700

    Added Page class to support LRU, LFU, NRU, algorithms

---
+++ Page.java
@@ -0,0 +1,27 @@
+
+public class Page {
+	
+	int address;
+	int counter;
+	
+	public Page(int address)
+	{
+		this.address = address;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+}
