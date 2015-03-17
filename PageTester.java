b08d6ca1b606e5427704eb347c09753cf1729eaf
mrjbar <jabari.lofton@sjsu.edu>
2015-03-16 17:19:01 -0700

    Prints out hit ratio of all algorithms

--- PageTester.java
+++ PageTester.java
@@ -7,15 +7,24 @@ public class PageTester {
 		 // Testing the nextPage method
 		 System.out.println("First In First Out");
 		 System.out.println("---------------------------------------------------------------------------------");
-		 p.fifo();
+		 double fifo = p.fifo();
 		 System.out.println("\n\n");
 		 System.out.println("Least Recently Used");
 		 System.out.println("---------------------------------------------------------------------------------");
-		 p.lru();
+		 double lru = p.lru();
+		 System.out.println("\n\n");
+		 System.out.println("Least Frequently Used");
+		 System.out.println("---------------------------------------------------------------------------------");
+		 double lfu = p.lfu();
+		 System.out.println("\n\n");
+		 System.out.println("Most Frequently Used");
+		 System.out.println("---------------------------------------------------------------------------------");
+		 double mfu = p.mfu();
 		 System.out.println("\n\n");
 		 System.out.println("Pick Random");
 		 System.out.println("---------------------------------------------------------------------------------");
-		 p.randomPick();
-
+		 double randomPick = p.randomPick();
+		 System.out.println("\nFIFO\tLRU\tLFU\tMFU\tRandom Pick");
+		 System.out.printf("%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", fifo, lru, lfu, mfu, randomPick);
 	}
 }
